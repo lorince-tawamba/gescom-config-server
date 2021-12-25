f254314997fc7e6ac5eb7be357ecdb5583fe95cb
lorincet <lorincet@gmail.com>
2021-12-25 19:48:38 +0100

    Initial commit

---
+++ src/test/java/tech/satic/gescom/configserver/GescomConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.satic.gescom.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GescomConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
